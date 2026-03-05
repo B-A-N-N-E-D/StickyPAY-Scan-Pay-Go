70fe8915dbe3df1a31579f9f2cffd68b3f259944
B-A-N-N-E-D <gamer01.pro.0000@gmail.com>
2026-03-05 19:59:51 +0530

    fixed vite base path

--- StickyPAY-Frontend/vite.config.js
+++ StickyPAY-Frontend/vite.config.js
@@ -4,10 +4,13 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
+  base: "./",
+
   plugins: [
     myPlugin(),
     react(),
   ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
