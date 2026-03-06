12c953f70f52df91cd106a7e7528459425f7c2e2
B-A-N-N-E-D <gamer01.pro.0000@gmail.com>
2026-03-06 22:55:59 +0530

    fixed database table name

--- StickyPAY-Backend/server.js
+++ StickyPAY-Backend/server.js
@@ -18,7 +18,7 @@ app.use(cors({
 app.use(express.json());
 
 // ── Routes
-app.use("/api/users", userRoutes);
+app.use("/api/profiles", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/scan", scanRoutes);
 
