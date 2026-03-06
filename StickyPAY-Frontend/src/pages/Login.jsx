941f57698cf764883fe285a1d98c1b4a7eee69dc
B-A-N-N-E-D <gamer01.pro.0000@gmail.com>
2026-03-06 23:23:14 +0530

    changes in Barcode.jsx & Login.jsx

--- StickyPAY-Frontend/src/pages/Login.jsx
+++ StickyPAY-Frontend/src/pages/Login.jsx
@@ -23,7 +23,7 @@ export default function Login() {
 
         setLoading(true);
         try {
-            const res = await fetch(`${import.meta.env.VITE_API_URL}/api/users/login`, {
+            const res = await fetch(`${import.meta.env.VITE_API_URL}/api/profiles/login`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(form)
